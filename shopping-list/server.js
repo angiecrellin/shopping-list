5313e9db7ff6e026ae410b039215a10fed06afea
Angie Crellin <angiecrellin@gmail.com>
2016-08-21 01:17:27 +0000

    -A

---
+++ shopping-list/server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+
+
+var Storage = function() {
+    this.items = [];
+    this.id = 0;
+};
+
+Storage.prototype.add = function(name) {
+    var item = {name: name, id: this.id};
+    this.items.push(item);
+    this.id += 1;
+    return item;
+};
+
+var storage = new Storage();
+storage.add('Broad beans');
+storage.add('Tomatoes');
+storage.add('Peppers');
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/items', function(request, response) {
+    response.json(storage.items);
+});
+
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+app.post('/items', jsonParser, function(request, response) {
+    if (!request.body) {
+        return response.sendStatus(400);
+    }
+
+    var item = storage.add(request.body.name);
+    response.status(201).json(item);
+});
+
+app.delete('/items/<id>',jsonParser, function(request,response) {
+    if (!request.body) {
+        return response.sendStatus(400);
+    }
+    
+    var item = storage.remove(request.body.name);
+    response.status(201).json(item);
+});
+
+app.listen(process.env.PORT, process.env.IP);
