2cef6ce8f39c601d4902900f5c135f6fe43e6185
Angie Crellin <angiecrellin@gmail.com>
2016-09-10 23:22:39 +0000

    -A

--- mongo-shopping-list/config.js
+++ mongo-shopping-list/config.js
@@ -1,6 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+exports.DATABASE_URL = 'mongodb://<dbuser>:<dbpassword>@ds029106.mlab.com:29106/shopping-list' 
 exports.PORT = process.env.PORT || 8080;
--- passport/user-model.js
+++ passport/user-model.js
@@ -11,93 +11,19 @@ var UserSchema = new mongoose.Schema({
     }
 });
 
+var bcrypt = require('bcryptjs');
+
+UserSchema.methods.validatePassword = function(password, callback) {
+    bcrypt.compare(password, this.password, function(err, isValid) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        callback(null, isValid);
+    });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = User;
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var User = require('./user-model');
-
-var app = express();
-
-var jsonParser = bodyParser.json();
-
-app.post('/users', jsonParser, function(req, res) {
-    if (!req.body) {
-        return res.status(400).json({
-            message: "No request body"
-        });
-    }
-
-    if (!('username' in req.body)) {
-        return res.status(422).json({
-            message: 'Missing field: username'
-        });
-    }
-
-    var username = req.body.username;
-
-    if (typeof username !== 'string') {
-        return res.status(422).json({
-            message: 'Incorrect field type: username'
-        });
-    }
-
-    username = username.trim();
-
-    if (username === '') {
-        return res.status(422).json({
-            message: 'Incorrect field length: username'
-        });
-    }
-
-    if (!('password' in req.body)) {
-        return res.status(422).json({
-            message: 'Missing field: password'
-        });
-    }
-
-    var password = req.body.password;
-
-    if (typeof password !== 'string') {
-        return res.status(422).json({
-            message: 'Incorrect field type: password'
-        });
-    }
-
-    password = password.trim();
-
-    if (password === '') {
-        return res.status(422).json({
-            message: 'Incorrect field length: password'
-        });
-    }
-
-    var user = new User({
-        username: username,
-        password: password
-    });
-
-    user.save(function(err) {
-        if (err) {
-            return res.status(500).json({
-                message: 'Internal server error'
-            });
-        }
-
-        return res.status(201).json({});
-    });
-});
-
-mongoose.connect('mongodb://localhost/auth').then(function() {
-    app.listen(process.env.PORT || 8080);
-});
-
-var User = require('./user-model');
-mongoose.connect('mongodb://localhost/auth').then(function() {
-    User.find(function(err, users) {
-        console.log(users);
-    });
-});
