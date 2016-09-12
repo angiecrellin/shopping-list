b9608c6d0947fc954a98b2f47e36a72bdf1aecd2
Angie Crellin <angiecrellin@gmail.com>
2016-09-12 18:05:41 +0000

    -A

--- spotify-recommend/server.js
+++ spotify-recommend/server.js
@@ -5,15 +5,15 @@ var events = require('events');
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
     unirest.get('https://api.spotify.com/v1/' + endpoint)
-           .qs(args)
-           .end(function(response) {
-                if (response.ok) {
-                    emitter.emit('end', response.body);
-                }
-                else {
-                    emitter.emit('error', response.code);
-                }
-            });
+        .qs(args)
+        .end(function(response) {
+            if (response.ok) {
+                emitter.emit('end', response.body);
+            }
+            else {
+                emitter.emit('error', response.code);
+            }
+        });
     return emitter;
 };
 var app = express();
@@ -25,46 +25,57 @@ app.get('/search/:name', function(req, res) {
         limit: 1,
         type: 'artist'
     });
+    
+    
 
     searchReq.on('end', function(item) {
         var artist = item.artists.items[0];
-        res.json(artist);
+    
+        var relatedArtist = getFromApi('artists/' + artist.id + '/related-artists', {
+            id: artist.id
+        });
+        var completedRequests = 0
+        var checkComplete = function(){
+            if (completedRequests === artist.related.length){
+                res.json(artist)
+            }
+        }
+        relatedArtist.on('end', function(relatedArtists) {
+            artist.related = relatedArtists.artists
+            artist.related.forEach(function(relatedArtist) {
+                
+                var topTracks = getFromApi('artists/' + relatedArtist.id + '/top-tracks?country=US', {
+                    id: relatedArtist.id
+                });
+
+                topTracks.on('end', function(item) {
+                    relatedArtist.tracks = item.tracks
+                    completedRequests++
+                    checkComplete();
+                });
+                
+
+                topTracks.on('error', function(code) {
+            
+                    completedRequests++
+                    checkComplete();
+                });
+
+            })
+            
+            
+        });
+        relatedArtist.on('error', function(code) {
+            res.sendStatus(code);
+        });
     });
 
     searchReq.on('error', function(code) {
         res.sendStatus(code);
     });
-});
 
-app.get('/artists/{id}/related-artists/:name', function(req,res){
-    var relatedArtist = getFromApi('artists', {
-        id: 'id'
-    });
-    relatedArtist.on('end', function(artist){
-        var artistsRelated = artist.artistsRelated.id;
-        res.json(artistsRelated);
-    });
-     relatedArtist.on('error', function(code) {
-        res.sendStatus(code);
-    });
 });
 
 
-app.get('artists/{id}/top-tracks/:name', function(req, res) {
-    var topTracks = getFromApi('tracks', {
-        q: 'country'
-    
-        
-    });
-
-    topTracks.on('end', function(tracks) {
-        var artist = tracks.artists.country;
-        res.json(topTracks);
-    });
-
-    topTracks.on('error', function(code) {
-        res.sendStatus(code);
-    });
-});
 
 app.listen(process.env.PORT || 8080);
